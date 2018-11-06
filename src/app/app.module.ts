6bec5859f1996586eb96c4f196650fba298482f8
Rutwik Uday Shete <rutwik.shete@gmail.com>
2018-11-06 18:37:04 +0530

    First Commit With Home page Worgin Without Buttons

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule , NO_ERRORS_SCHEMA} from '@angular/core';
+
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
